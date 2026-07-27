f0223d54c65cc0704fc34e102a17958a292bac82
Pouriya <pouriyamohammadbeigi46@gmail.com>
2026-07-27 16:33:49 +0330

    added static and directory and urls  in setting
    + static root
    + static url
    +media root
    +media url

--- practice/settings.py
+++ practice/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website.apps.WebsiteConfig'
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = [BASE_DIR / "media"]
