490dbf8c026ecbabeb76cbdd125ffe41bbf87b1d
Pouriya <pouriyamohammadbeigi46@gmail.com>
2026-08-11 18:47:49 +0330

    Project Template
    added Project Template  in django Project

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path ('index', index_view),
-    path ('about', about_view),
-    path ('contact', contact_view)
+    path ('', index_view, name = 'index'),
 ]
--- website/views.py
+++ website/views.py
@@ -5,9 +5,3 @@ from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
     return render (request, "website/index.html")
-
-def about_view(request):
-    return render (request, "website/about.html")
-
-def contact_view(request):
-    return render (request, "website/contact.html")
